8fc9e700993300ad8b26edafe051523dfa078094
Remiel <82504896@qq.com>
2013-03-01 17:50:14 +0800

    gdpt

--- centosRT.sh
+++ centosRT.sh
@@ -1,8 +1,34 @@
+#----Apache2的是作为一个CentOS的软件包，因此我们可以直接用下面命令安装它：
+#
+ #   yum install httpd
+#
+#　　现在配置系统在引导时启动Apache
+
+ #   chkconfig --levels 235 httpd on
+
+　#　并启动Apache
+
+   # /etc/init.d/httpd start
+
+    #Apache的默认文档根目录是在CentOS上的/var/www/html 目录 ，配置文件是/etc/httpd/conf/httpd.conf。配置存储在的/etc/httpd/conf.d/目录。
+
+  #---安装PHP5
+
+   # 我们可以用下面的命令来安装PHP5
+
+    #  yum install php
+
+    #安装完需要重启
+
+  #  /etc/init.d/httpd restart原文地址:http://server.zol.com.cn/279/2797239.html 
+
+#-----
+
 yum install mod_dav_svn subversion
 
 yum install gcc gcc-c++ m4 make automake libtool pkgconfig perl openssl-devel ncurses-devel mod_ssl openssl-devel apache2 php php-cli screen
 
-cd /tmp
+cd /tmp;
 svn checkout http://xmlrpc-c.svn.sourceforge.net/svnroot/xmlrpc-c/stable xmlrpc-c
 wget http://curl.haxx.se/download/curl-7.19.7.tar.gz
 wget http://ftp.gnome.org/pub/GNOME/sources/libsigc++/2.2/libsigc++-2.2.4.tar.gz
@@ -60,3 +86,18 @@ cd ~
 
 wget http://libtorrent.rakshasa.no/export/1105/trunk/rtorrent/doc/rtorrent.rc
 mv rtorrent.rc .rtorrent.rc
+
+vi .rtorrent.rc
+#     scgi_port = 127.0.0.1:5000
+
+
+cd /var/www/
+svn co http://rutorrent.googlecode.com/svn/trunk/rutorrent
+
+
+cd /var/www/rutorrent/plugins
+svn co http://rutorrent.googlecode.com/svn/trunk/plugins/rpc
+
+
+
+screen rtorrent
